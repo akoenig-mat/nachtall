d62d99278110c7bdb46b54956e4bfbb0178da6f9
akoenig-mat <akoenig@media-art-theory.com>
2021-12-02 14:57:24 +0100

    Jetson Indexer

--- fileindexer.py
+++ fileindexer.py
@@ -3,9 +3,9 @@ import pandas as pd
 
 files = glob.glob('./recordings/*.*')
 
-folder_index = pd.DataFrame(files)
-folder_index.to_csv('./dataframes/recordings.csv')
+folder_index = pd.DataFrame(files, columns=['wav_filename'])
+folder_index.to_csv('./dataframes/recordings.csv', index=False)
 
-folder_index
+
 
 
